8a0eae8017a18dffe27c463126f15915aa9f8cc3
andri-irawan79 <iandri647@gmail.com>
2022-04-12 14:10:17 +0800

    berhasil menampilkan data 1

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,15 +1,40 @@
 <template>
   <v-app>
-    <tampilan-utama />
+    <navbar-menu />
+    <v-main>
+      <v-row>
+        <v-col :cols="menuValue ? '2': '0'" :class="menuValue ? 'd-inline-block': 'd-none'">
+          <sidebar-menu />
+        </v-col>
+        <v-col :cols="menuValue ? '10': '12'">
+          <v-container>
+            <Nuxt />
+          </v-container>
+        </v-col>
+      </v-row>
+    </v-main>
+    <footer-page />
   </v-app>
 </template>
 
 <script>
-  import TampilanUtama from '~/components/TampilanUtama.vue'
-  export default {
-    name: 'HomeView',
-    components: {
-      TampilanUtama,
-    },
-  }
+import FooterPage from '~/components/FooterPage.vue';
+import SidebarMenu from '~/components/SidebarMenu.vue';
+import NavbarMenu from '~/components/NavbarMenu.vue'
+// import TampilanUtama from '~/components/TampilanUtama.vue';
+
+export default {
+  name: 'App',
+  components: { 
+    SidebarMenu, 
+    FooterPage, 
+    NavbarMenu,
+    // TampilanUtama,
+  },
+  computed: {
+    menuValue(){
+      return this.$store.state.sidebar;
+    }
+  },
+};
 </script>
